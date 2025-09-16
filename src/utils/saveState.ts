1e36b062d6efe0c881151488d92dfa08297b0229
dborcas <209258582+dborcas@users.noreply.github.com>
2025-09-16 13:24:48 -0700

    Remove unused saveState function

--- src/utils/saveState.ts
+++ src/utils/saveState.ts
@@ -1,11 +1,5 @@
 import { getErrorMessage } from "./error.ts";
 
-
-export const saveState = <T, >(key: string, state: T) => {
-	const json = JSON.stringify(state);
-	localStorage.setItem(key, JSON.stringify(json));
-};
-
 export const restoreState = <T, >(key: string): Nullable<T> => {
 	const json = localStorage.getItem(key);
 	if (!json) {
