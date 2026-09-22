cbc16556faf8ba1d8efdabc7f3ec8e100ba85139
dborcas <209258582+dborcas@users.noreply.github.com>
2026-09-22 08:17:13 -0700

    Add current date for cache-breaking

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import react from "@vitejs/plugin-react";
 import * as path from "node:path";
 import { defineConfig } from "vitest/config";
 import packageJson from "./package.json" with { type: "json" };
+import banner from 'vite-plugin-banner'
+const buildDate = new Date().toUTCString();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        banner(`/*! Build Date: ${new Date().toISOString()} */`)
+    ],
 
     server: {
         open: true,
@@ -18,13 +23,15 @@ export default defineConfig({
             output: {
                 entryFileNames: "assets/[name].js",
                 assetFileNames: "assets/[name].[ext]",
+                banner: `/* Build Date: ${buildDate} */`,
             },
             input: {
                 index: "index.html",
                 worker: "src-worker/index.ts"
-            }
+            },
         },
     },
+
     test: {
         root: import.meta.dirname,
         name: packageJson.name,
